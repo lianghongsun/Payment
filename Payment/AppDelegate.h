b91efa4a5b86c51a963dd9333696ab7d18f8e044
噗噗 <pupu@promote.cache-dns.local>
2018-03-31 18:03:41 +0800

    Initial Commit

---
+++ Payment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Payment
+//
+//  Created by 噗噗 on 2018/3/31.
+//  Copyright © 2018年 噗噗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Payment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Payment
+//
+//  Created by 噗噗 on 2018/3/31.
+//  Copyright © 2018年 噗噗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
